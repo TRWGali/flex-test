20c964405953ed1ff272be04caf8ceb3ec83272f
Taylor White <34773851+TRWGali@users.noreply.github.com>
2020-10-01 09:35:44 -0400

    'embed' flex-ui into react (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as Flex from '@twilio/flex-ui';
 
-function App() {
+interface IProps {
+  manager: Flex.Manager
+}
+
+function App(props: IProps) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Flex.ContextProvider manager={props.manager}>
+          <Flex.RootContainer />
+        </Flex.ContextProvider>
+
       </header>
     </div>
   );
